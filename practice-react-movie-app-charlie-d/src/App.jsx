dc36ecb00a185c5e048a3c92ccb34515b8b89a52
CharlieDavidson <charliedavidson4011@gmail.com>
2025-11-03 09:25:09 -0600

    final push

---
+++ practice-react-movie-app-charlie-d/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MovieSelector from "./MovieSelector";
+
+function App() {
+    return (
+        <div className="App">
+            <MovieSelector />
+        </div>
+    );
+}
+
+export default App;
